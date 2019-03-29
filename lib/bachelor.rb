446bc5daaa8bc59ad73c323f94366be1a90381f5
Sarah Elzinga <sevans924@gmail.com>
2019-03-29 16:11:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,6 +45,7 @@ def get_average_age_for_season(data, season)
   age_array = []
     data[season].each do |contestant|
       age_array.push(contestant["age"].to_i)
+      binding.pry
     end# code here
     age_array.average
 end
