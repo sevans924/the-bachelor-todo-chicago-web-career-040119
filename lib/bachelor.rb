d7a99101b6e2373a28e83781e1aee88672282b38
Sarah Elzinga <sevans924@gmail.com>
2019-03-29 15:51:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -13,6 +13,7 @@ def get_contestant_name(data, occupation)
       if cont_data["occupation"].downcase == occupation
          return contestant["name"]
        end
+       end
      end# code here
 end
 
