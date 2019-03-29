9ae271fe55f5442265874f14c9b694f93707d18c
Sarah Elzinga <sevans924@gmail.com>
2019-03-29 15:45:46 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,7 +2,7 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
       if contestant["status"].downcase == "winner"
          return contestant["name"].split(" ").first
-      
+
     end
   end# code here
 end
