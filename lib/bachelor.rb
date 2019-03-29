d0b687a4f87158287f8b9f127031e3412a45f40a
Sarah Elzinga <sevans924@gmail.com>
2019-03-29 16:48:31 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
+
+#require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestant|
+      if contestant["status"].downcase == "winner"
+         return contestant["name"].split(" ").first
+
+    end
+  end# code here
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |cont_data|
+      if cont_data["occupation"] == occupation
+         return cont_data["name"]
+       end
+       end
+     end# code here
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, contestant|
+    contestant.each do |cont_data|
+      if cont_data["hometown"] == hometown
+        counter += 1# code here
+      end
+    end
+  end
+counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |cont_data|
+      if cont_data["hometown"] == hometown
+         return cont_data["occupation"]
+       end
+    end
+  end# code here
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+age_array = []
+    data[season].each do |contestant|
+      age_array << contestant["age"].to_i
+
+    #  contestant.each do |cont_data|
+    #end# code here
+  end
+  (age_array.reduce(:+) / age_array.size.to_f).round(0) 
+
 end
